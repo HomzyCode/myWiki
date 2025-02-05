1a3e002a0c62146196749e2452f89179e92184ea
HomzyCode <marcusfadairo777@gmail.com>
2025-02-04 22:26:29 -0800

    edit page and save_edit

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
     path("search/", views.search, name="search"),
-    path("new/", views.new_page, name="new_page")
+    path("new/", views.new_page, name="new_page"),
+    path("edit/", views.edit, name="edit"),
+    path("save_edit/", views.save_edit, name="save_edit")
 ]
---
+++ entries/Test Page.md
@@ -0,0 +1,5 @@
+# Test Page
+
+This is a test page!!
+
+I will continue to add stuff to this new page!!
